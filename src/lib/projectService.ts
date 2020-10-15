bd96f536de246ab423b32ac5449c978e5c4bfa00
Teun Zengerink <t.zengerink@gmail.com>
2020-10-15 16:28:26 +0200

    Moved projects API to static file

--- src/lib/projectService.ts
+++ src/lib/projectService.ts
@@ -14,5 +14,7 @@ export interface Project {
 }
 
 export const getProjects = (): Promise<Project[]> => {
-  return axios.get('http://localhost:3000/api/projects').then((response) => response.data.projects)
+  return axios
+    .get(`${process.env.NEXT_PUBLIC_PROTO}://${process.env.NEXT_PUBLIC_HOST}/projects.json`)
+    .then((response) => response.data.projects)
 }
