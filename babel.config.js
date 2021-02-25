481f86d5190c78de9918db09b1dbb3cd22c81bcd
Teun Zengerink <t.zengerink@gmail.com>
2021-02-25 09:55:35 +0100

    Configure smoke test to run after deploy
    By using the latest commit hash as the build ID, it becomes possible to
    check whether or not the latest version of `main` has been deployed to
    Github pages.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript', '@babel/preset-react', 'next/babel'],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+    'next/babel',
+  ],
 }
