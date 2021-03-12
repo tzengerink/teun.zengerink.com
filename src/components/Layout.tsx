72a28729bd92c58cc4450a27071b8ea30f22965d
Teun Zengerink <t.zengerink@gmail.com>
2021-03-12 09:48:42 +0100

    Refactored how page and site title work

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,8 @@ import { Project } from '../lib/projects'
 import styles from './Layout.module.scss'
 import Sidebar from './Sidebar'
 
-export const pageTitle = 'Teun Zengerink'
+const TITLE_ROOT = 'Teun Zengerink'
+const TITLE_DIV = ' - '
 
 interface LayoutProps {
   title?: string
@@ -12,22 +13,23 @@ interface LayoutProps {
   children?: React.ReactNode
 }
 
-const Layout = (props: LayoutProps): React.ReactElement => {
-  const title = React.useMemo(() => (props.title ? props.title : pageTitle), [props.title, pageTitle])
+const Layout: React.FC<LayoutProps> = ({ title, projects, children }): React.ReactElement => {
+  const pageTitle = React.useMemo(() => (title ? [TITLE_ROOT, title].join(TITLE_DIV) : TITLE_ROOT), [title])
+
   return (
     <div>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="description" content={title}></meta>
-        <title>{title}</title>
+        <meta name="description" content={pageTitle}></meta>
+        <title>{pageTitle}</title>
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,500;1,300&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
-      <Sidebar pageTitle={pageTitle} projects={props.projects} />
-      <main className={styles.main}>{props.children}</main>
+      <Sidebar pageTitle={TITLE_ROOT} projects={projects} />
+      <main className={styles.main}>{children}</main>
     </div>
   )
 }
