cec6f6236bb31291ac12861366e63a3a6b87424f
Teun Zengerink <t.zengerink@gmail.com>
2021-03-17 14:46:09 +0100

    Moved Loader to Layout

--- src/pages/404.test.tsx
+++ src/pages/404.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import projects from '../__mocks__/projects'
-import Error404 from './404.page'
+import Error from './404.page'
 
-const defaultProps = { projects }
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Error404 {...defaultProps} />).toJSON()
-  expect(tree).toMatchSnapshot()
+describe('Error', () => {
+  it('renders correctly', () => {
+    const props = { projects }
+    const tree = renderer.create(<Error {...props} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
-import Loader from '../components/Loader/Loader'
 import SinglePhoto from '../components/SinglePhoto/SinglePhoto'
 import { Props } from '../lib/static'
 
 const Home: React.FC<Props> = ({ projects }): React.ReactElement => {
-  return <Layout projects={projects}>{projects?.length ? <SinglePhoto project={projects[0]} /> : <Loader />}</Layout>
+  return (
+    <Layout projects={projects}>
+      <SinglePhoto project={projects[0]} />
+    </Layout>
+  )
 }
 
 export { getStaticProps } from '../lib/static'
