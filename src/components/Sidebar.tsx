a60cd74d500149f59504d1f902e461308c12a427
Teun Zengerink <t.zengerink@gmail.com>
2021-03-17 08:16:28 +0100

    Refactor Sidebar into multiple components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,52 +1,95 @@
 import React, { useState } from 'react'
-import { useRouter, NextRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { Project } from '../lib/projects'
 import styles from './Sidebar.module.scss'
+
+interface SocialAccount {
+  name: string
+  url: string
+  image: string
+}
 
 interface SidebarProps {
   projects: Project[]
   pageTitle: string
 }
 
-const isActiveProject = (router: NextRouter, project: Project): boolean => {
-  if (!router?.query?.slug) return false
-  return router.query?.slug[0] === project.slug
+interface ClickableProps {
+  onClick: () => void
 }
 
-const Sidebar = (props: SidebarProps): React.ReactElement => {
+interface MenuItemProps extends ClickableProps {
+  project: Project
+}
+
+interface SocialsProps {
+  accounts: SocialAccount[]
+}
+
+const ACCOUNTS: SocialAccount[] = [
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com/t.zengerink/',
+    image: '/img/instagram.jpg',
+  },
+  {
+    name: 'Pinterest',
+    url: 'https://pinterest.com/tzengerink/',
+    image: '/img/pinterest.jpg',
+  },
+]
+
+const Hamburger: React.FC<ClickableProps> = ({ onClick }) => {
+  return (
+    <div className={styles.hamburger} onClick={onClick}>
+      <span className={styles.line}></span>
+      <span className={styles.line}></span>
+      <span className={styles.line}></span>
+    </div>
+  )
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ project, onClick }) => {
   const router = useRouter()
+  const isActive = router?.query?.slug ? router.query.slug[0] === project.slug : false
+
+  return (
+    <li key={project.slug} className={isActive ? styles.active : ''}>
+      <Link href={`/work/${project.slug}${!project.statement ? `/${project.photos[0].key}` : ''}`}>
+        <a onClick={onClick}>{project.title}</a>
+      </Link>
+    </li>
+  )
+}
+
+const Socials: React.FC<SocialsProps> = ({ accounts }) => (
+  <span className={styles.socials}>
+    {accounts.map((account) => (
+      <a key={account.name} href={account.url}>
+        <img src={account.image} alt={account.name} />
+      </a>
+    ))}
+  </span>
+)
+
+const Sidebar: React.FC<SidebarProps> = ({ pageTitle, projects }): React.ReactElement => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <aside className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
       <a href="/">
-        <h1>{props.pageTitle}</h1>
+        <h1>{pageTitle}</h1>
       </a>
-      <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
-        <span className={styles.line}></span>
-        <span className={styles.line}></span>
-        <span className={styles.line}></span>
-      </div>
+      <Hamburger onClick={() => setIsOpen(!isOpen)} />
       <nav className={styles.navigation}>
-        {props.projects?.length ? <h2>Work</h2> : ''}
+        {projects?.length ? <h2>Work</h2> : null}
         <ul>
-          {props.projects?.map((project) => (
-            <li key={project.slug} className={isActiveProject(router, project) ? styles.active : ''}>
-              <Link href={`/work/${project.slug}${!project.statement ? `/${project.photos[0].key}` : ''}`}>
-                <a onClick={() => setIsOpen(false)}>{project.title}</a>
-              </Link>
-            </li>
+          {projects?.map((project) => (
+            <MenuItem key={project.slug} project={project} onClick={() => setIsOpen(false)} />
           ))}
         </ul>
-        <span className={styles.socials}>
-          <a href="https://instagram.com/t.zengerink/">
-            <img src="/img/instagram.jpg" alt="Instagram" />
-          </a>
-          <a href="https://pinterest.com/tzengerink/">
-            <img src="/img/pinterest.jpg" alt="Pinterest" />
-          </a>
-        </span>
+        <Socials accounts={ACCOUNTS} />
       </nav>
     </aside>
   )
