322a629fc8fc5eebf0c145deabf28b09c33b502f
Teun Zengerink <t.zengerink@gmail.com>
2021-03-18 15:37:57 +0100

    Improvement of test coverage

--- src/components/Sidebar/Sidebar.test.tsx
+++ src/components/Sidebar/Sidebar.test.tsx
@@ -1,11 +1,26 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import React from 'react'
 import projects from '../../__mocks__/projects'
 import Sidebar from './Sidebar'
 
 const defaultProps = { pageTitle: 'My Homepage', projects }
 
-it('renders correctly', () => {
-  const { container } = render(<Sidebar {...defaultProps} />)
-  expect(container).toMatchSnapshot()
+describe('Sidebar', () => {
+  const renderComponent = (props = defaultProps) => render(<Sidebar {...props} />)
+
+  it('renders correctly', () => {
+    const { container } = renderComponent()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('toggles the menu when the hamburger is clicked', () => {
+    const { getByTestId } = renderComponent()
+    const sidebar = getByTestId('sidebar')
+    const hamburger = getByTestId('hamburger')
+    expect(sidebar.classList.contains('open')).toBe(false)
+    fireEvent.click(hamburger)
+    expect(sidebar.classList.contains('open')).toBe(true)
+    fireEvent.click(hamburger)
+    expect(sidebar.classList.contains('open')).toBe(false)
+  })
 })
