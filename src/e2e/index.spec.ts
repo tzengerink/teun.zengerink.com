8fe38af1bf0ea3e83c29be0a231a3a265bb166ed
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2022-02-07 17:22:36 +0100

    Improve the speed of the e2e tests

--- src/e2e/index.spec.ts
+++ src/e2e/index.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { getActiveProjects, titleDiv, titleRoot } from './helpers'
 
 test.beforeEach(async ({ page }) => {
--- src/lib/static.test.ts
+++ src/lib/static.test.ts
@@ -1,14 +1,13 @@
 import mockProjects from '../__mocks__/projects'
 import { getStaticPaths, getStaticProps } from './static'
 
-const mockGetProjects = jest.fn(() => new Promise((resolve) => resolve(mockProjects)))
-
-jest.mock('./projects', () => ({ getProjects: () => mockGetProjects() }))
+jest.mock('./projects', () => ({
+  createGetProjects: () => () => Promise.resolve(mockProjects),
+}))
 
 describe('getStaticProps', () => {
   it('should get the props', async () => {
     const props = await getStaticProps()
-    expect(mockGetProjects).toHaveBeenCalled()
     expect(props).toEqual({ props: { projects: mockProjects } })
   })
 })
@@ -18,7 +17,6 @@ describe('getStaticPaths', () => {
     const numberOfProjectsWithStatement = mockProjects.filter((project) => !!project.statement).length
     const numberOfPhotos = mockProjects.reduce((sum, project) => sum + project.photos.length, 0)
     const paths = await getStaticPaths()
-    expect(mockGetProjects).toHaveBeenCalled()
     expect(paths.fallback).toBe(false)
     expect(paths.paths.length).toEqual(numberOfProjectsWithStatement + numberOfPhotos)
   })
