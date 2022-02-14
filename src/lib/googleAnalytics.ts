4787896e7a82b552591c99e02d986ab78591fdd6
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2022-02-14 17:00:57 +0100

    Log page tracking to console when id not set

--- src/lib/googleAnalytics.ts
+++ src/lib/googleAnalytics.ts
@@ -5,7 +5,7 @@ declare global {
 }
 
 export const trackPageView = (url: string): void => {
-  if (!process.env.NEXT_PUBLIC_TRACKING_ID) return
+  if (!process.env.NEXT_PUBLIC_TRACKING_ID) return console.info(`[Tracking Pageview] ${url}`)
   window.gtag('config', process.env.NEXT_PUBLIC_TRACKING_ID, {
     page_path: url,
   })
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,9 +5,8 @@ import '../styles/globals.css'
 
 const App: React.FC<AppProps> = ({ Component, router, pageProps }) => {
   useEffect(() => {
-    const handleRouteChange = (url: string) => trackPageView(url)
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => router.events.off('routeChangeComplete', handleRouteChange)
+    router.events.on('routeChangeComplete', trackPageView)
+    return () => router.events.off('routeChangeComplete', trackPageView)
   }, [router.events])
 
   return <Component {...pageProps} />
