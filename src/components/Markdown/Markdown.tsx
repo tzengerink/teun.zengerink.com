43afd569383899ae8d4614477d8afa563a3daa45
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2022-04-13 13:24:00 +0200

    Define `children` prop for v18

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { marked } from 'marked'
 import classNames from 'classnames'
 
+interface Props {
+  children: React.ReactNode
+}
+
 const toInnerHtml = (
   markdown: string,
 ): {
   __html: string
 } => ({ __html: marked.parse(markdown) })
 
-const Markdown: React.FC = ({ children }) => {
+const Markdown: React.FC<Props> = ({ children }) => {
   return (
     <div
       className={classNames('space-y-sm', 'text-xs', 'text-justify')}
