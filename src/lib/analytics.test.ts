23108da40efbfde0f7c9cd46f4ba4bb0dbc4001a
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2022-05-12 16:14:34 +0200

    Remove analytics logic

--- src/lib/analytics.test.ts
+++
@@ -1,9 +0,0 @@
-import { trackPageView } from './analytics'
-
-describe('trackPageView', () => {
-  test('logs to the console', () => {
-    jest.spyOn(console, 'info').mockImplementationOnce(() => undefined)
-    trackPageView()
-    expect(console.info).toHaveBeenCalled()
-  })
-})
