99e2b0197a5e43f29d1c95f3eec48971ae9628fa
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2022-05-12 14:58:55 +0200

    Replace Google Analytics with Piwik Pro

---
+++ src/components/PiwikProScript/PiwikProScript.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react'
+import { PiwikProScript } from './PiwikProScript'
+
+describe('PiwikProScript', () => {
+  it('renders a Piwik Pro script tag', () => {
+    const { container } = render(
+      <PiwikProScript host={process.env.NEXT_PUBLIC_PIWIK_HOST} appId={process.env.NEXT_PUBLIC_PIWIK_APP_ID} />,
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('renders nothing when no app configured', () => {
+    const { container } = render(<PiwikProScript host={process.env.NEXT_PUBLIC_PIWIK_HOST} appId="" />)
+    expect(container).toMatchSnapshot()
+  })
+})
