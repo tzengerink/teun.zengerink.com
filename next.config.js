69719d8f465dd66a4f56c5f0f894877bba8d4e1c
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2023-08-01 09:15:24 +0200

    Use export configuration instead of command

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const nextBuildId = require('next-build-id')
 
 module.exports = {
+  output: 'export',
   generateBuildId: () => nextBuildId({ dir: __dirname }),
   pageExtensions: ['page.tsx'],
   webpack: (config, { isServer }) => {
