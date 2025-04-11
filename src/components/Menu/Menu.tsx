cb829e13e659c3702e65664873d65cf1a2eba0b7
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-04-11 11:51:11 +0200

    Resolve issue with menu/statement on touch devices

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -6,55 +6,62 @@ import MenuItem from '../MenuItem/MenuItem'
 interface MenuProps {
   projects: Project[]
   isOpen: boolean
+  onOpen: () => void
   onItemClick: () => void
 }
 
-const Menu: React.FC<MenuProps> = ({ projects, isOpen, onItemClick }) => (
-  <nav
-    data-testid="menu"
-    className={classNames(
-      'group',
-      'fixed',
-      'z-30',
-      'inset-y-0',
-      'left-xs',
-      'w-full',
-      'p-md',
-      'pt-xl',
-      'bg-transparant-white',
-      'ease-linear',
-      'duration-100',
-      { 'translate-y-0': isOpen, 'opacity-100': isOpen, visible: isOpen },
-      { 'translate-y-[-20px]': !isOpen, 'opacity-0': !isOpen, invisible: !isOpen },
-      'md:visible',
-      'md:opacity-100',
-      'md:top-md',
-      'md:right-md',
-      'md:bottom-auto',
-      'md:left-auto',
-      'md:w-auto',
-      'md:p-0',
-      'md:pt-md',
-      'md:text-right',
-    )}
-  >
-    <h2 className={classNames('my-xs', 'uppercase', 'text-xs', 'text-grey/80', 'cursor-default')}>Projects</h2>
-    <ul
+const Menu: React.FC<MenuProps> = ({ projects, isOpen, onOpen, onItemClick }) => {
+  return (
+    <nav
+      data-testid="menu"
       className={classNames(
-        'leading-5',
-        'md:ease-linear',
-        'md:duration-100',
+        'group',
+        'fixed',
+        'z-30',
+        'inset-y-0',
+        'left-xs',
+        'w-full',
+        'p-md',
+        'pt-xl',
+        'bg-transparant-white',
+        'ease-linear',
+        'duration-100',
+        { 'translate-y-0': isOpen, 'opacity-100': isOpen, visible: isOpen },
+        { 'translate-y-[-20px]': !isOpen, 'opacity-0': !isOpen, invisible: !isOpen },
+        'md:translate-y-0',
         'md:translate-y-[-20px]',
-        'md:invisible',
-        'md:group-hover:visible',
-        'md:group-hover:translate-y-0',
+        'md:visible',
+        'md:opacity-100',
+        'md:top-md',
+        'md:right-md',
+        'md:bottom-auto',
+        'md:left-auto',
+        'md:w-auto',
+        'md:p-0',
+        'md:pt-md',
+        'md:text-right',
       )}
     >
-      {projects.map((project) => (
-        <MenuItem key={project.slug} project={project} onClick={onItemClick} />
-      ))}
-    </ul>
-  </nav>
-)
+      <h2 className={classNames('my-xs', 'uppercase', 'text-xs', 'text-grey/80', 'cursor-default')} onClick={onOpen}>
+        Projects
+      </h2>
+      <ul
+        className={classNames(
+          'leading-5',
+          { invisible: !isOpen },
+          { 'ease-linear': !isOpen },
+          { 'duration-100': !isOpen },
+          { 'translate-y-[-20px]': !isOpen },
+          { 'group-hover:visible': !isOpen },
+          { 'group-hover:translate-y-0': !isOpen },
+        )}
+      >
+        {projects.map((project) => (
+          <MenuItem key={project.slug} project={project} onClick={onItemClick} />
+        ))}
+      </ul>
+    </nav>
+  )
+}
 
 export default Menu
--- src/components/Statement/Statement.tsx
+++ src/components/Statement/Statement.tsx
@@ -1,41 +1,49 @@
 import classNames from 'classnames'
 import Markdown from '../Markdown/Markdown'
+import { useState } from 'react'
 
 interface StatementProps {
   title: string
   statement: string
 }
 
-const Statement: React.FC<StatementProps> = ({ title, statement }) => (
-  <div className={classNames('group', 'text-grey/80', 'select-none')}>
-    <h2 className={classNames('fixed', 'left-md', 'bottom-md', 'z-20', 'text-xs', 'uppercase', 'cursor-help')}>
-      Statement
-    </h2>
-    <div
-      className={classNames(
-        'fixed',
-        'left-0',
-        'bottom-md',
-        'z-10',
-        'right-0',
-        'mb-md',
-        'p-md',
-        'bg-transparant-white',
-        'invisible',
-        'opacity-0',
-        'ease-linear',
-        'duration-100',
-        'translate-y-[50px]',
-        'group-hover:visible',
-        'group-hover:translate-y-0',
-        'group-hover:opacity-100',
-        'md:w-[500px]',
-      )}
-    >
-      <h3 className={classNames('mb-sm', 'text-sm')}>{title}</h3>
-      <Markdown>{statement}</Markdown>
+const Statement: React.FC<StatementProps> = ({ title, statement }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className={classNames('group', 'text-grey/80', 'select-none')}>
+      <h2
+        className={classNames('fixed', 'left-md', 'bottom-md', 'z-20', 'text-xs', 'uppercase', 'cursor-help')}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Statement
+      </h2>
+      <div
+        className={classNames(
+          'fixed',
+          'left-0',
+          'bottom-md',
+          'z-10',
+          'right-0',
+          'mb-md',
+          'p-md',
+          'bg-transparant-white',
+          { invisible: !isOpen },
+          { 'opacity-0': !isOpen },
+          'ease-linear',
+          'duration-100',
+          { 'translate-y-[50px]': !isOpen },
+          'group-hover:visible',
+          'group-hover:translate-y-0',
+          'group-hover:opacity-100',
+          'md:w-[500px]',
+        )}
+      >
+        <h3 className={classNames('mb-sm', 'text-sm')}>{title}</h3>
+        <Markdown>{statement}</Markdown>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Statement
