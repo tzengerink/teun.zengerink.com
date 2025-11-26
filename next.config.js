03f2c698ca1348ab9f859ace804abfc0ae1a7fbb
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-11-26 15:58:38 +0100

    Removed output export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 import nextBuildId from 'next-build-id'
 
 export default {
-  output: 'export',
   generateBuildId: () => nextBuildId({ dir: new URL('.', import.meta.url).pathname }),
   devIndicators: false,
 }
